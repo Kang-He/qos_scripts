e257d3bc83883477011b8152e1a4ab01b81dc3b4
yushua <yushua@node-10.11qos.cloudincr-pg0.wisc.cloudlab.us>
2021-06-17 03:23:38 -0500

    修改了自动设置

--- config.sh
+++ config.sh
@@ -32,11 +32,32 @@ echo "set ip of server to $server_ip"
 echo "set ip of redis to $redis_ip"
 echo "set ip of monitor to $monitor_ip"
 echo "set name of experiment to $exp_name"
+# 设置serve.vol
 sed -i "s/redis_ip/$redis_ip/" ./server.vol
 sed -i "s/server_ip/$server_ip/" ./server.vol
+# 设置client.vol
+sed -i "s/redis_ip/$redis_ip/" ./client.vol
+sed -i "s/server_ip/$server_ip/" ./client.vol
+# 设置prometheus.yml
+sed -i "s/server_ip/$server_ip/" ./prometheus.yml
+# 设置cpu_register.sh
 sed -i "s/monitor_ip/$monitor_ip/" ./cpu_register.sh
 sed -i "s/exp_name/$exp_name/" ./cpu_register.sh
+# 设置cpu_delete.sh
 sed -i "s/monitor_ip/$monitor_ip/" ./cpu_delete.sh
 sed -i "s/exp_name/$exp_name/" ./cpu_delete.sh
+# 设置qos.sh
 sed -i "s/monitor_ip/$monitor_ip/" ./qos.sh
+# 设置configs.py
+sed -i "s/redis_ip/$redis_ip/" ./configs.py
+sed -i "s/server_ip/$server_ip/" ./configs.py
+sed -i "s/monitor_ip/$monitor_ip/" ./configs.py
+sed -i "s/exp_name/$exp_name/" ./configs.py
+# 设置tree.html
+sed -i "s/monitor_ip/$monitor_ip/" ./tree.html
+# 设置template.html
+sed -i "s/monitor_ip/$monitor_ip/" ./template.html
+# 设置redis.conf
+sed -i 's/daemonize no/daemonize yes/' ./redis.conf
+sed -i "s/bind 127.0.0.1/bind $redis_ip/" ./redis.conf
 exit 0
---
+++ configs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+REDIS_DB_URL = {
+    'host': 'redis_ip',
+    'port': 6379,
+    'password': '',
+    'db': 0
+}
+
+PUBLISH_CHANNEL = "jy-test"
+SUBSCRIBE_SERVER_CHANNEL = "server"
+SUBSCRIBE_CLIENT_CHANNEL = "client"
+DELIMER = "^^"
+
+set_keys = ['app_rbw', 'app_wbw', 'app_data_iops', 'app_create_iops', 'app_open_iops', 'app_unlink_iops', 'app_stat_iops']
+
+keys = ['app_rbw', 'app_wbw', 'app_data_iops', 'app_create_iops', 'app_open_iops', 'app_unlink_iops', 'app_stat_iops'
+        , 'app_w_delay', 'app_r_delay', 'app_name']
+
+# 提供计算节点与存储节点的对应关系
+fix=".exp_name.cloudincr-pg0.wisc.cloudlab.us"
+server_list=['server_ip']
+compute_dict = {'node-0'+fix: server_list,
+                'node-1'+fix: server_list}
+
+HOST='monitor_ip'
+# HOST='0.0.0.0'
